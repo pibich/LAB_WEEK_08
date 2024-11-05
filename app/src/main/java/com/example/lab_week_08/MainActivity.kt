fc28a18b83de229bbbc7669e74eb626337d8dceb
PIBI <76818508+pibich@users.noreply.github.com>
2024-11-05 13:34:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -0,0 +1,159 @@
+package com.example.lab_week_08
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
+import com.example.lab_week_08.worker.ThirdWorker
+
+class MainActivity : AppCompatActivity() {
+
+    private val workManager = WorkManager.getInstance(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestNotificationPermission()
+        } else {
+            startWork()
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    NOTIFICATION_PERMISSION_CODE
+                )
+            } else {
+                startWork()
+            }
+        } else {
+            // Langsung lanjutkan ke startWork untuk versi Android di bawah API 33
+            startWork()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == NOTIFICATION_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Izin diberikan
+                Toast.makeText(this, "Notification permission granted", Toast.LENGTH_SHORT).show()
+                startWork()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Notification permission denied. Notifications won't be shown.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun startWork() {
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest.Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val secondRequest = OneTimeWorkRequest.Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val thirdRequest = OneTimeWorkRequest.Builder(ThirdWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(ThirdWorker.INPUT_DATA_ID, id))
+            .build()
+
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .then(thirdRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("First process is done")
+                }
+            }
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("Second process is done")
+                    launchNotificationService()
+                }
+            }
+        workManager.getWorkInfoByIdLiveData(thirdRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("Third process is done")
+                    launchSecondNotificationService()
+                }
+            }
+    }
+
+    private fun getIdInputData(idKey: String, idValue: String) =
+        Data.Builder()
+            .putString(idKey, idValue)
+            .build()
+
+    private fun showResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun launchNotificationService() {
+        NotificationService.trackingCompletion.observe(this) { id ->
+            showResult("Process for Notification Channel ID $id is done!")
+        }
+        val serviceIntent = Intent(this, NotificationService::class.java).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    private fun launchSecondNotificationService() {
+        SecondNotificationService.trackingCompletion.observe(this) { id ->
+            showResult("Process for Notification Channel ID $id is done!")
+        }
+        val serviceIntent = Intent(this, SecondNotificationService::class.java).apply {
+            putExtra(EXTRA_ID, "002")
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    companion object {
+        const val EXTRA_ID = "id"
+        const val NOTIFICATION_PERMISSION_CODE = 1
+    }
+}
---
+++ app/src/main/java/com/example/lab_week_08/NotificationService.kt
@@ -0,0 +1,128 @@
+package com.example.lab_week_08
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.IBinder
+import android.os.Looper
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class NotificationService : Service() {
+
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var serviceHandler: Handler
+
+    override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notificationBuilder = startForegroundService()
+
+        val handlerThread = HandlerThread("SecondThread")
+            .apply { start() }
+        serviceHandler = Handler(handlerThread.looper)
+    }
+
+    private fun startForegroundService(): NotificationCompat.Builder {
+        val pendingIntent = getPendingIntent()
+        val channelId = createNotificationChannel()
+        val notificationBuilder = getNotificationBuilder(pendingIntent, channelId)
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        return notificationBuilder
+    }
+
+    private fun getPendingIntent(): PendingIntent {
+        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            PendingIntent.FLAG_IMMUTABLE else 0
+
+        return PendingIntent.getActivity(
+            this, 0, Intent(this, MainActivity::class.java), flag
+        )
+    }
+
+    private fun getNotificationBuilder(pendingIntent: PendingIntent, channelId: String) =
+        NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Second Worker process is done")
+            .setContentText("Check it out!")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setTicker("Second worker process is done, check it out!")
+            .setOngoing(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setAutoCancel(true)
+
+    private fun createNotificationChannel(): String =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "001"
+            val channelName = "001 Channel"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Notification channel for background process updates"
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                vibrationPattern = longArrayOf(100, 200, 300, 400, 500)
+            }
+
+            val service = requireNotNull(
+                ContextCompat.getSystemService(this, NotificationManager::class.java)
+            )
+            service.createNotificationChannel(channel)
+            channelId
+        } else { "" }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val returnValue = super.onStartCommand(intent, flags, startId)
+        val id = intent?.getStringExtra(EXTRA_ID)
+            ?: throw IllegalStateException("Channel ID must be provided")
+
+        serviceHandler.post {
+            countDownFromTenToZero(notificationBuilder)
+            notifyCompletion(id)
+            stopForeground(STOP_FOREGROUND_REMOVE)
+            stopSelf()
+        }
+        return returnValue
+    }
+
+    private fun countDownFromTenToZero(notificationBuilder: NotificationCompat.Builder) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        for (i in 10 downTo 0) {
+            Thread.sleep(1000L)
+            notificationBuilder.setContentText("$i seconds until last warning")
+                .setSilent(true)
+            notificationManager.notify(
+                NOTIFICATION_ID,
+                notificationBuilder.build()
+            )
+        }
+    }
+
+    private fun notifyCompletion(id: String) {
+        Handler(Looper.getMainLooper()).post {
+            mutableID.value = id
+        }
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 0xCA7
+        const val EXTRA_ID = "id"
+        private val mutableID = MutableLiveData<String>()
+        val trackingCompletion: LiveData<String> = mutableID
+    }
+}
